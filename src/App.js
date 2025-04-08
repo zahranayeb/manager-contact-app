4d653099f5a3354c3d4d8654a437c3bc7e1ba02a
zahranayeb <zahra_nayebloo@yahoo.com>
2025-04-08 13:08:58 +0330

    FirstCommit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h3>Manage Contact App</h3>
+      <button className='btn btn-primary data-bs-toggle'>Contact</button>
+    </div>
+  );
+}
+
+export default App;
