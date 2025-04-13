ba823ac32a240641c7062dc086cdbf67406f5e13
zahranayeb <zahra_nayebloo@yahoo.com>
2025-04-13 09:17:32 +0330

    new

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 const App = () => {
   return (
     <div className="App">
-      <h3 style={{ fontFamily: "Vazir" }}>اپلیکیشن مدیریت مخاطبین</h3>
+      {/* <h3 style={{ fontFamily: "Vazir" }}>اپلیکیشن مدیریت</h3> */}
       <button className='btn btn-primary data-bs-toggle'>Contact</button>
       <br />
       <span className='bx bxs-user'></span>
