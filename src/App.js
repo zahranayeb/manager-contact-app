871c0675779a3af681d7f06a7d115a55cb03f4b8
zahranayeb <zahra_nayebloo@yahoo.com>
2025-05-19 11:55:57 +0330

    new

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { useState } from 'react';
 import Navbar from './components/Navbar';
 import Contacts from './components/Contacts';
 import Example from './components/Example';
-import Test from './components/test';
 
 const App = () => {
 
@@ -11,9 +10,7 @@ const App = () => {
 
   return (
     <div className="App">
-      <Test/>
       <Example />
-      
       <Navbar />
       <Contacts contacts={getContacts} />
 
